d2ebeddfd546286b1e61e96e5b5cf2c87f62fa3e
Coffee <svenvonnatzmer@outlook.de>
2024-11-23 13:47:24 +0000

    anpassungen

--- middleware/auth.js
+++
@@ -1,6 +0,0 @@
-export default function ({ $auth, redirect }) {
-    // Überprüfen, ob der Nutzer eingeloggt ist
-    if (!$auth.loggedIn) {
-      return redirect('/login'); // Weiterleitung zur Login-Seite
-    }
-  }
